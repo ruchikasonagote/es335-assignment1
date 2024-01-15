13adadf665b8f5649aa24aeded10d2b8f981b8fd
ruchikasonagote <ruchikasonagote2003@gmail.com>
2024-01-15 21:46:49 +0530

    1st update on utils.py

--- tree/utils.py
+++ tree/utils.py
@@ -3,23 +3,25 @@ You can add your own functions here according to your decision tree implementati
 There is no restriction on following the below template, these fucntions are here to simply help you.
 """
 
+import numpy as np
 import pandas as pd
-
+from pd.api.types import is_numeric_dtype
 
 def check_ifreal(y: pd.Series) -> bool:
-    """
-    Function to check if the given series has real or discrete values
-    """
+    # Function to check if the given series has real or discrete values
+    is_real = is_numeric_dtype(y)
 
-    pass
+    return is_real
 
 
 def entropy(Y: pd.Series) -> float:
-    """
-    Function to calculate the entropy
-    """
-
-    pass
+    # Function to calculate the entropy
+    # unique values in Y
+    unique_values = Y.value_counts()
+    # probability of each value in unique_values
+    norm = unique_values/unique_values.sum()
+    # entropy of Y
+    return norm.apply(lambda x: -x * np.log2(x + 1e-6)).sum()
 
 
 def gini_index(Y: pd.Series) -> float:
